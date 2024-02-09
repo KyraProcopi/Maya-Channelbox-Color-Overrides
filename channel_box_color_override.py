67ed6341c53cd7a0859901f5a3a8d8c3323811f6
KyraProcopi <148473162+KyraProcopi@users.noreply.github.com>
2024-02-09 13:10:06 -0800

    Update channel_box_color_override.py
    Update with UI

--- channel_box_color_override.py
+++ channel_box_color_override.py
@@ -6,26 +6,21 @@ from maya import cmds, OpenMayaUI
 main_channelbox_name = "mainChannelBox"
 node_data_code = "main_channelbox_name = " + """'mainChannelBox'"""
 
-def channelColorData():
-    if cmds.objExists("channelColorData"):
-        channelBoxColorsData = "channelColorData"
-        return channelBoxColorsData
-    else:
-        channelBoxColorsData = cmds.scriptNode(n="channelColorData", st=1)
-        cmds.setAttr("channelColorData.sourceType", 1)
-        return channelBoxColorsData 
-
-def get_existing_data():
-    if cmds.objExists("channelColorData"):
-        get_existing_data = cmds.scriptNode("channelColorData", bs=True, q=True)
-        return get_existing_data 
-
 #Change channel BG box color
-def channelBoxColorOverride():
+def channelBoxColorOverride(code):
+    
     #Find pyside widget
     channelbox_widget = wrapInstance(int(OpenMayaUI.MQtUtil.findControl(main_channelbox_name)),QtWidgets.QWidget)
     selected_attributes = cmds.channelBox(main_channelbox_name, q=True, sma=True)
-   
+    
+    #Script Node
+    if cmds.objExists("channelColorData"):
+        channelBoxColorsData = "channelColorData"
+    else:
+        channelBoxColorsData = cmds.scriptNode(n="channelColorData", st=1)
+        cmds.setAttr("channelColorData.sourceType", 1)
+    
+    #Change Color
     def color_change(selected_attribute):
         cmds.channelBox(
         main_channelbox_name,
@@ -40,7 +35,7 @@ def channelBoxColorOverride():
             """attrBgColor={})""".format(values)+ """\n""") 
         color_data_code = str(color_data)
         print(color_data_code)
-        return color_data_code  
+        return color_data_code 
    
     if selected_attributes==None:
         print("Please select attributes in channel box")
@@ -53,20 +48,58 @@ def channelBoxColorOverride():
             #Loop through 
             color_output = str()
             color_data = color_output.join(color_change(attr)for attr in selected_attributes)
-            return color_data
 
         else:
             print('Editor was dismissed')
                       
-#Plug Code intro Script Node
-channelColorData()  
-color_data = channelBoxColorOverride()
+    #Plug Code intro Script Node 
+    if cmds.objExists("channelColorData"):
+        code = cmds.scriptNode("channelColorData", bs=True, q=True)
+    
+    if color_data != None:
+        try:
+            code += "\n" + color_data
+            cmds.scriptNode("channelColorData", edit=True, bs=code)
+            
+        except:
+            code = node_data_code + "\n" + color_data
+            cmds.scriptNode("channelColorData", edit=True, bs=code)
+                          
+#Select Script Node
+def channelColorData(code):
+    if cmds.objExists("channelColorData"):
+        print("Node Exists")
+        channelBoxColorsData = "channelColorData"
+        cmds.select("channelColorData")
+        return channelBoxColorsData
+    else:
+        print("Script node has not been created. Please apply color overrides to create script node")
 
-if color_data != None:
-    try:
-        code += "\n" + color_data
+#Get Exsiting data         
+def get_existing_data(code):
+    if cmds.objExists("channelColorData"):
+        code = cmds.scriptNode("channelColorData", bs=True, q=True)
+        print("Exisiting Override Data retrieved") 
+        return code
+    else:
+        print("No existing data")        
+
+#Reset Overrides
+def reset(code):
+    if cmds.objExists("channelColorData"):
+        code = node_data_code
         cmds.scriptNode("channelColorData", edit=True, bs=code)
-        
-    except:
-        code = node_data_code + "\n" + color_data
-        cmds.scriptNode("channelColorData", edit=True, bs=code)
+        print("Overrides restored to default. Please close Maya and re-open")
+    
+    else:
+        print("Script node has not been created. Please apply color overrides to create script node")
+                
+
+#UI
+cmds.window(menuBar=True, width=255, h=50, s=False)
+cmds.columnLayout( columnAttach=('both', 5), rowSpacing=10, columnWidth=250 )
+cmds.button('Change Color', c=channelBoxColorOverride)
+cmds.button('Select Script Node', c=channelColorData)
+cmds.button('Get Existing Data', c=get_existing_data)
+cmds.button('Reset All Overrides',c=reset)
+cmds.showWindow()
