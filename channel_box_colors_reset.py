b57584e0cca7ed1d7e3a95f18178b0a3e5d06c50
KyraProcopi <148473162+KyraProcopi@users.noreply.github.com>
2024-02-09 13:11:11 -0800

    Update channel_box_colors_reset.py

--- channel_box_colors_reset.py
+++ channel_box_colors_reset.py
@@ -1,6 +1,1 @@
-#Global Variables 
-main_channelbox_name = "mainChannelBox"
-node_data_code = "main_channelbox_name = " + """'mainChannelBox'"""
 
-code = node_data_code
-cmds.scriptNode("channelColorData", edit=True, bs=code)
